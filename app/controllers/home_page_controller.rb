1f8b3f8bd3f9ff6dcf416391a62dbeb163686c19
wellnitm <wellnitm@lafayette.edu>
2017-11-08 15:22:51 -0500

    First pass at controllers, some work on css

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,7 @@
 class HomePageController < ApplicationController
   def index
+    @counties =School.uniq.pluck(:county)
+    @districts =District.uniq.pluck(:lea_name)
+    @schools =School.uniq.pluck(:school_name)
   end
 end
