cc4c6e2a7ad57d6e0248741319fbe297f1e9c495
wellnitm <wellnitm@lafayette.edu>
2017-11-13 19:57:48 -0500

    Auto stash before merge of "master" and "origin/master"

--- app/controllers/visualization2_controller.rb
+++ app/controllers/visualization2_controller.rb
@@ -3,7 +3,7 @@ class Visualization2Controller < ApplicationController
     @year = params[:year]
     @district = params[:district]
     if (@district != nil && @year != nil)
-      d = District.find_by_lea_name(@district).pluck(:nces_lea_id)
+      d = District.find_by_lea_name(@district).uniq.pluck(:nces_lea_id)
       s = School.where(nces_lea_id: d)
       @schools = s.uniq.pluck(:school_name)
       districtCohorts = 0
