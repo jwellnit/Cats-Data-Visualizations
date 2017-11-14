661c09c118d31156a8a8a8666a64b7854c1d7ecf
wellnitm <wellnitm@lafayette.edu>
2017-11-13 20:18:11 -0500

    updated vis3 controller

--- app/controllers/visualization3_controller.rb
+++ app/controllers/visualization3_controller.rb
@@ -6,14 +6,14 @@ class Visualization3Controller < ApplicationController
     if (@school1 != nil && @school2 != nil && @year != nil)
       s1 = School.find_by_school_name(@school1)
       s2 = School.find_by_school_name(@school2)
-      @attendanceRate1 = DataSchoolPerformanceMeasure.where(state_school_id: s1.state_school_id).where(academic_year_start: @year).uniq.pluck(:attendance_rate)
-      @attendanceRate2 = DataSchoolPerformanceMeasure.where(state_school_id: s2.state_school_id).where(academic_year_start: @year).uniq.pluck(:attendance_rate)
-      sc1 = DataMultiYearGradRate.select("total_cohort").where(state_school_id: s1.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take
-      sg1 = DataMultiYearGradRate.select("total_grads").where(state_school_id: s1.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take
-      @gradRate1 = sc1/sg1
-      sc2 = DataMultiYearGradRate.select("total_cohort").where(state_school_id: s2.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take
-      sg2 = DataMultiYearGradRate.select("total_grads").where(state_school_id: s2.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take
-      @gradRate2 = sc2/sg2
+      @attendanceRate1 = DataSchoolPerformanceMeasure.where(state_school_id: s1.state_school_id).where(academic_year_start: @year).uniq.take.attendance_rate
+      @attendanceRate2 = DataSchoolPerformanceMeasure.where(state_school_id: s2.state_school_id).where(academic_year_start: @year).uniq.take.attendance_rate
+      sc1 = DataMultiYearGradRate.where(state_school_id: s1.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take.total_cohort
+      sg1 = DataMultiYearGradRate.where(state_school_id: s1.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take.total_grads
+      @gradRate1 = (sg1/sc1)*100
+      sc2 = DataMultiYearGradRate.where(state_school_id: s2.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take.total_cohort
+      sg2 = DataMultiYearGradRate.where(state_school_id: s2.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take.total_grads
+      @gradRate2 = (sg2/sc2)*100
       @percLowIncome1 = 0.2
       @percLowIncome2 = 0.7
     end
