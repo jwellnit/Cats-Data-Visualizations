d8af60bef438871fad5ca2fc156bcee08e2ac68c
wellnitm <wellnitm@lafayette.edu>
2017-11-20 16:28:56 -0500

    Ordering on home page

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,8 +1,8 @@
 class HomePageController < ApplicationController
   def index
-    @counties =School.uniq.pluck(:county)
-    @districts =District.where("state_lea_id <> 0").uniq.pluck(:lea_name)
-    @schools =School.uniq.pluck(:school_name)
-    @years =DataMultiYearGradRate.uniq.pluck(:academic_year_start)
+    @counties =School.order(:county).uniq.pluck(:county)
+    @districts =District.where("state_lea_id <> 0").order(:lea_name).uniq.pluck(:lea_name)
+    @schools =School.order(:school_name).uniq.pluck(:school_name)
+    @years =DataMultiYearGradRate.order(:academic_year_start).uniq.pluck(:academic_year_start)
   end
 end
