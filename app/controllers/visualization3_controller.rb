3b2b916703d4cd781145b6b820121b0a4ded2b6a
wellnitm <wellnitm@lafayette.edu>
2017-11-27 22:18:46 -0500

    vis 3 cont fix

--- app/controllers/visualization3_controller.rb
+++ app/controllers/visualization3_controller.rb
@@ -15,36 +15,36 @@ class Visualization3Controller < ApplicationController
         @attendanceRate1 = sa1.attendance_rate
         @attendanceRate2 = sa2.attendance_rate
       else
-        @attendanceRate1 = 0
-        @attendanceRate2 = 0
+        @attendanceRate1 = -1
+        @attendanceRate2 = -1
       end
       #school 1 grad rate
       sc1 = DataMultiYearGradRate.where(state_school_id: s1.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take
       if (sc1 != nil && sc1.total_cohort != nil)#check for nulls
         @gradRate1 = (sc1.total_grads.to_f/sc1.total_cohort.to_f)*100
       else
-        @gradRate1 = 0
+        @gradRate1 = -1
       end
       #school 2 grad rate
       sc2 = DataMultiYearGradRate.where(state_school_id: s2.state_school_id).where(academic_year_start: @year).where(time_period: 4).uniq.take
       if (sc2 != nil && sc2.total_cohort != nil)#check for nulls
         @gradRate2 = (sc2.total_grads.to_f/sc2.total_cohort.to_f)*100
       else
-        @gradRate2 = 0
+        @gradRate2 = -1
       end
       #school 1 low income rate
       li1 = DataPublicSchoolLowIncome.where(state_school_id: s1.state_school_id).where(academic_year_start: @year).uniq.take
       if (li1 != nil && li1.low_income_enrollment != nil && li1.enrollment != nil)#check for nulls
         @percLowIncome1 = li1.low_income_enrollment.to_f/li1.enrollment.to_f
       else
-        @percLowIncome1 = 0
+        @percLowIncome1 = -1
       end
       #school 2 low income rate
       li2 = DataPublicSchoolLowIncome.where(state_school_id: s2.state_school_id).where(academic_year_start: @year).uniq.take
       if (li2 != nil && li2.low_income_enrollment != nil && li2.enrollment != nil)#check for nulls
         @percLowIncome2 = li2.low_income_enrollment.to_f/li2.enrollment.to_f
       else
-        @percLowIncome2 = 0.2
+        @percLowIncome2 = -1
       end
     end
   end
